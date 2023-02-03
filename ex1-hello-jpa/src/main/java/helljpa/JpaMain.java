307a8062f1ff3425e3350c090c4266cdc794aed5
Seongmin <chr92@naver.com>
2023-02-03 16:24:16 +0900

    Section8-cascade

--- ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
@@ -15,24 +15,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Member member1 = new Member();
-            member1.setUsername("Member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("============================");
-            System.out.println("teamName = " + m.getTeam().getName());
-            System.out.println("============================");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
             tx.commit();
 
