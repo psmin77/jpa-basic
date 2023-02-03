c777f7923ac48900804fbb0fbf92c633e0160439
Seongmin <chr92@naver.com>
2023-02-03 15:57:37 +0900

    Section8-lazy-loading

--- ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
@@ -1,7 +1,5 @@
 package helljpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,18 +15,24 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member1 = new Member();
-            member1.setUsername("hello1");
+            member1.setUsername("Member1");
+            member1.setTeam(team);
             em.persist(member1);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("reference = " + refMember.getClass()); // Proxy
+            Member m = em.find(Member.class, member1.getId());
+            System.out.println("m = " + m.getTeam().getClass());
 
-            Hibernate.initialize(refMember); // 강제 초기화
-            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+            System.out.println("============================");
+            System.out.println("teamName = " + m.getTeam().getName());
+            System.out.println("============================");
 
             tx.commit();
 
