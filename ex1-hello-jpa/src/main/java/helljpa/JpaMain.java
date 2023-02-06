1eb5ac6f5c62af3c3f802aa043ec0a949b50a0e3
Seongmin <chr92@naver.com>
2023-02-06 09:58:39 +0900

    Section9-immutable object

--- ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
@@ -16,12 +16,15 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city","street","1000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "1000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
 
             tx.commit();
 
