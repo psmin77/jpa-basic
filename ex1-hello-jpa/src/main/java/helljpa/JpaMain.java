c99d06668c836b3b0711654b2fe757db3f75bbcc
Seongmin <chr92@naver.com>
2023-02-07 15:29:41 +0900

    Section10-1

--- ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/helljpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -20,34 +23,13 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "1000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "2000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "3000"));
-
             em.persist(member);
 
             em.flush();
-            em.clear();
 
-            System.out.println("========= START ===========");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            // 주소 변경
-//            Address old = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", old.getStreet(), old.getZipcode()));
-//
-//            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            // 주소 히스토리 수정
-//            findMember.getAddressHistory().add(new Address("old1", "street", "2000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "2000"));
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
 
             tx.commit();
 
