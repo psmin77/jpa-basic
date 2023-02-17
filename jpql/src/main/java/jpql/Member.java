5c9627bb330f11a8ade29f0b79abcde8281f0451
Seongmin <chr92@naver.com>
2023-02-17 15:08:33 +0900

    Section11-named-query

--- jpql/src/main/java/jpql/Member.java
+++ jpql/src/main/java/jpql/Member.java
@@ -1,8 +1,14 @@
 package jpql;
 
+import jdk.jfr.Name;
+
 import javax.persistence.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
